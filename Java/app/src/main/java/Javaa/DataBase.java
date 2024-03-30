43c83abfdb03b3eb7700526dc0a980e00a79d9b6
lauti2311 <lautaro_ismael2010@hotmail.com>
2024-03-30 13:09:05 -0300

    no se q se hizo xd

--- Java/app/src/main/java/Javaa/DataBase.java
+++ Java/app/src/main/java/Javaa/DataBase.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 //ACA HAY QUE CAMBIAR LOS DATOS DE LA BASE DE DATOS Bv
 
 public class DataBase {
-    private static final String URL = "jdbc:mysql://localhost:3306/tu_basededatos";
-    private static final String USER = "tu_usuario";
-    private static final String PASSWORD = "tu_contraseña";
+    private static final String URL = "jdbc:mysql://localhost:3306/fcaultad";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
