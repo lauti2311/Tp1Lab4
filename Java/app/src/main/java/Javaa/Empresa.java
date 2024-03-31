1776358f49c27f10becd0986ab04de75ce5752d3
lauti2311 <lautaro_ismael2010@hotmail.com>
2024-03-31 17:44:15 -0300

    Se corrige la conexión con la BD

--- Java/app/src/main/java/Javaa/Empresa.java
+++ Java/app/src/main/java/Javaa/Empresa.java
@@ -27,6 +27,78 @@ public class Empresa {
         this.email = email;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getHorarioAtencion() {
+        return horarioAtencion;
+    }
+
+    public void setHorarioAtencion(String horarioAtencion) {
+        this.horarioAtencion = horarioAtencion;
+    }
+
+    public String getQuienesSomos() {
+        return quienesSomos;
+    }
+
+    public void setQuienesSomos(String quienesSomos) {
+        this.quienesSomos = quienesSomos;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     // Getters y Setters
 }
 
